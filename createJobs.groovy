acc68c50362545f46567374e5aa04edff8896dfd
Vu Nguyen <unreal136@gmail.com>
2020-07-18 21:32:16 -0700

    jenkins boot

---
+++ createJobs.groovy
@@ -0,0 +1,23 @@
+pipelineJob('pipelineJob') {
+    definition {
+        cps {
+            script(readFileFromWorkspace('pipelineJob.groovy'))
+            sandbox()
+        }
+    }
+}
+
+pipelineJob('theme-park-job') {
+    definition {
+        cpsScm {
+            scm {
+                git {
+                    remote {
+                        url 'https://github.com/vungu/spring-boot-api-ex.git'
+                    }
+                    branch 'master'
+                }
+            }
+        }
+    }
+}
